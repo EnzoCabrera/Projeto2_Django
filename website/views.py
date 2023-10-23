edc10857d63bb4393969d1f014d6ce9ddec10ae8
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-10-23 11:51:39 -0300

    handling website aesthetics

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from . forms import SignUpForm
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request, "There was an error loggin in!!, please try again...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 def login_user(request):
     pass
@@ -45,4 +47,13 @@ def register_user(request):
     
     return render(request, 'register.html', {'form':form})
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
+
+        
     
